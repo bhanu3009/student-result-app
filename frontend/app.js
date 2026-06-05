298c7ec71d8ce10355542b53a88194a5db090992
bhanu3009 <bhanuprakasht588@gmail.com>
2026-06-05 18:41:50 +0530

    Day 7: Connected frontend to live Render backend URL

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 // Automatically switch between your local server and your future live cloud server
 const API_BASE_URL = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost" 
     ? "http://127.0.0.1:8000" 
-    : "https://your-future-backend-url.onrender.com"; // We will update this exact URL later today!
+    : "https://student-result-backend.onrender.com"; // We will update this exact URL later today!
 
 // ==========================================
 // 1. LOGIN PAGE LOGIC (index.html)
